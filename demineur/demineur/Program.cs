778e4881d5601a2ff87ecb7a4005643b84b15a8b
othman.elbaz <othman.elbaz@epita.fr>
2020-08-25 18:54:33 +0100

    deminer

---
+++ demineur/demineur/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace demineur
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            if(Int32.TryParse(args[0],out number) && Int32.TryParse(args[1],out number))
+            {
+                var a = new game(Int32.Parse(args[0]),Int32.Parse(args[1]));
+                a.ChooseWhatToDO();
+            }
+        }
+    }
+}
